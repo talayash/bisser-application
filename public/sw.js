6288ff34e236bfb66def52a63a75ceedc9b889ed
Tal Ayash <50268009+talayash@users.noreply.github.com>
2022-11-07 09:09:34 +0200

    Icon and Setting

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./src/master.css", "./images/icon2.png"]);
+            return cache.addAll(["./", "./src/master.css", "./images/icon.png"]);
         })
     );
 });
